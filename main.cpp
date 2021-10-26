b4f97037680fe1197e85db3026b2a413dee04356
girayaThiago <giraya.jap@gmail.com>
2021-10-26 11:52:26 -0300

    G(r), H(r), print vec

--- main.cpp
+++ main.cpp
@@ -12,17 +12,14 @@ int main (){
     srand(std::random_device()());
     // string mensagem = "The quick brown fox jumps over the lazy dog (tradução do inglês para A rápida raposa marrom pula por cima do cão preguiçoso) é um pangrama, frase que utiliza todas as letras do alfabeto em língua inglesa.\nAlguns tradutores de programas simplesmente transliteram a citação sem procurar usar um pangrama em português, como \"Um pequeno jabuti xereta viu dez cegonhas felizes\"";
     std::string mensagem = "The quick brown fox jumps over the lazy dog";
-    // std::cout << sha3_256(mensagem) << std::endl;
     RSA_Class rsa;
-    // unsigned char out[SHA256_DIGEST_LENGTH];
-
-    // rsa.simpleSHA256((void *)mensagem.c_str(), mensagem.length(), out);
     
-    // std::string mgf1 = rsa.sha3_256(mensagem);
-    std::vector<uint8_t> mgf1 = rsa.G(mensagem);
-    cout << mgf1.size() << endl;
-    cout << rsa.bytes_to_hex_string(mgf1) << endl;
+    // std::vector<uint8_t> mgf1 = rsa.G(mensagem);
+    // cout << mgf1.size() << endl;
+    // cout << rsa.bytes_to_hex_string(mgf1) << endl;
     printf("\n");
-    // std::pair<RSA_Private_Key, RSA_Public_Key> keys = rsa.generate_keys();
+    
+    std::pair<RSA_Private_Key, RSA_Public_Key> keys = rsa.generate_keys();
+    rsa.encrypt(keys.first, keys.second, mensagem);
     return 0;
 }
--- rsa.hpp
+++ rsa.hpp
@@ -26,20 +26,20 @@ private:
     int1024 get_lambda(int1024 p, int1024 q);
     int1024 get_d(int1024 e, int1024 lambda_n);
     int1024 gcd(int1024 a, int1024 b);
-    std::string get_salt(int k0);
+    std::vector<uint8_t>  get_salt(int k0);
     std::string expand_salt(const std::string &salt);
     std::string read_text(const std::string &path);
+    void print_vec_uint8(const std::vector<uint8_t> &v);
 public:
-    // std::string sha3_256(const std::string& input);
-    std::vector<uint8_t> sha3_256(const std::string& input);
+    std::vector<uint8_t> sha3_256(const std::vector<uint8_t>& input);
     std::pair<RSA_Private_Key, RSA_Public_Key>  generate_keys();
     std::string encrypt(const RSA_Private_Key &p,const RSA_Public_Key &q,const std::string &mensagem);
     std::string decrypt(const RSA_Private_Key &p,const RSA_Public_Key &q,const std::string &mensagem);
     std::string padding(const std::string &mensagem, int k0, int k1);
-    // std::string G(const std::string& r);
 
-    std::vector<uint8_t> G(const std::string& r);
+    std::vector<uint8_t> hash(const std::vector<uint8_t>& r, int length);
     std::string bytes_to_hex_string(const std::vector<uint8_t>& bytes);
+    std::vector<uint8_t> xor_vec(const std::vector<uint8_t>&a,const std::vector<uint8_t> &b);
 };
 
 
