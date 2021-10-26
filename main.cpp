180e32b4211774c0f956296e40bdb2ca2bb89406
girayaThiago <giraya.jap@gmail.com>
2021-10-26 20:23:43 -0300

    alterado int1024 para wrapper CBigNum

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 
 int main (){
-
-    srand(std::random_device()());
+    // srand(std::random_device()());
     // string mensagem = "The quick brown fox jumps over the lazy dog (tradução do inglês para A rápida raposa marrom pula por cima do cão preguiçoso) é um pangrama, frase que utiliza todas as letras do alfabeto em língua inglesa.\nAlguns tradutores de programas simplesmente transliteram a citação sem procurar usar um pangrama em português, como \"Um pequeno jabuti xereta viu dez cegonhas felizes\"";
     std::string mensagem = "The quick brown fox jumps over the lazy dog";
     RSA_Class rsa;
